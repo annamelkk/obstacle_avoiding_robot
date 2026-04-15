164769e38eedd5ffbd9309b80376c23b16b608da
Anna Melqonanyan <melqonanyanna@gmail.com>
2026-04-15 22:45:47 +0400

    after fail

--- 05-prof_temp/05-prof_temp.ino
+++ 05-prof_temp/05-prof_temp.ino
@@ -13,60 +13,44 @@
 #define PIN_TRIG          11
 #define BIT_TRIG          11  // D11 - P411
 #define BIT_ECHO          3   // D10 - P103
-#define OBSTACLE_DISTANCE 20
+#define OBSTACLE_DISTANCE 30
 
 // LDR
 #define PIN_LDR       A0
 #define BLACK         495
-#define DEADBAND      5
-#define THRESHOLD     500
-#define WHITE         730
+#define WHITE         780
+#define THRESHOLD     ((BLACK + WHITE)/2)
 
+#define SMOOT_FACTOR 5
+#define DEADBAND     5
 
-#define LINE_LOST_W   600
-#define LINE_LOST_B   420
-
-
-#define SEARCH_SPEED  100
-#define DRIVE_SPEED   190
-#define RECOVER_SPEED 110
+#define SEARCH_SPEED  120
+#define DRIVE_SPEED   180
+#define RECOVER_SPEED 100
 
 // PID controller values
-#define k_p           0.45
-
-#define k_i           0.01
-#define anti_windup   35.0
-
-#define k_d           0.1
-
-// sensor poll interval
-#define SENSOR_POLL_MS 60UL
-
-// recovery timeout - prevents infinite spin
-#define RECOVER_TIMEOUT_MS 600UL
+#define k_p           0.6
+#define k_i           0.0
+#define k_d           0.2
 
 // ====================== GLOBALS =========================
 
-float error = 0.0;
-float prev_error = 0.0;
-float integral = 0.0;
-float derivative = 0.0;
+// on off
+int last_turn = 0;
+int prev_state = 1; // assume starting black
+int turn_strength = 100;
 
-float filtered_sensor = 0.0;
-float alpha = 0.25;
-
-unsigned long prev_time = 0;
-int last_turn = 1;
-
-unsigned int last_sensor_time = 0;
-int           cached_distance = 999;
-
-// recovery state
-bool          recovering = false;
-unsigned long recover_start_time = 0;
-
+// pid
+float error = 0;
+float prev_error = 0;
+float integral = 0;
 
 // ====================== SETUP ===========================
+
+/*
+ * We are using poert registers to directly manipulate Arduino uno R4 Renesas MCUs memory,
+ * according to Google this method is around 30x faster than digitalWrite() and pinMode()
+ */
 void setup()
 {
   Serial.begin(9600);
@@ -74,131 +58,94 @@ void setup()
   // motor direction pins – all on PORT1 in Renesas datasheet
   R_PORT1->PDR  |=  (1 << BIT_IN1) | (1 << BIT_IN2) |
                     (1 << BIT_IN3) | (1 << BIT_IN4);
+  // start LOW
   R_PORT1->PODR &= ~((1 << BIT_IN1) | (1 << BIT_IN2) |
                      (1 << BIT_IN3) | (1 << BIT_IN4));
 
-  // Ultrasonic sensor
+  // ultrasonic sensor
   R_PORT4->PDR  |=  (1 << BIT_TRIG); // TRIG output – PORT4 bit 11
   R_PORT1->PDR  &= ~(1 << BIT_ECHO); // ECHO input  – PORT1 bit 3
   R_PORT4->PODR &= ~(1 << BIT_TRIG); // TRIG starts LOW
 
-  // PWM enable pins
+  // PWM enable pins, using pinmode for analogWrite()
   pinMode(PIN_ENA, OUTPUT);
   pinMode(PIN_ENB, OUTPUT);
 
-  int first_read = analogRead(PIN_LDR);
-  filtered_sensor = first_read;
-
-  prev_time = micros();
-  last_sensor_time = millis(); 
   delay(1000); // 1 sec delay to place the robot in position
 }
 
 // ====================== LOOP ============================
 void loop()
 {
-  // read distance
-  if (millis() - last_sensor_time >= SENSOR_POLL_MS)
-  {
-    cached_distance  = get_distance_cm();
-    last_sensor_time = millis();
-  }
- // if obstacle is detected stop everythign
-  if (cached_distance < OBSTACLE_DISTANCE)
-  {
-    stop();
-    return;
-  }
-  else
-  {
-    // read ldr
-    int ldr = analogRead(PIN_LDR);
-    Serial.print("LDR value: "); Serial.println(ldr);
+  int ldr = ldr_smoothing();
+  
+  //ON_OFF_CTRL(ldr);
+  PID_CTRL(ldr);
+  
 
-    PID_CTRL();     // p-control function where motor PWM is proportional to error
-  }
 }
 
 // ====================== CONTROL FUNCTIONS ===============
 
-void PID_CTRL() {
-  int sensor_val = read_filtered_sensor(); // smoothing out noise from the LDR
-
-  unsigned long now = micros();
-  float dt = (now - prev_time) / 1000000.0;
-  prev_time = now;
-
-  if (dt <= 0.0) dt = 0.001;
-  if (dt > 0.05) dt = 0.05;
-
-  if (sensor_val >= LINE_LOST_W || sensor_val <= LINE_LOST_B) 
-  {
-    if(!recovering)
-    {
-      recovering = true;
-      recover_start_time = millis();
-    }
-  if (millis() - recover_start_time > RECOVER_TIMEOUT_MS)
-  {
-    // spin in place more aggressively
-    drive(last_turn * -140, last_turn * 140);
-  }
-  else
-      search_line();
-
-    // reset PID state so stale values don't spike on re-acquisition
-    integral   = 0.0f;
-    prev_error = 0.0f;
-    return;
-  }
-
-  recovering = false;
-  
-  // classic PID iplementation
-  error = (float)(THRESHOLD - sensor_val);
-
-  if (abs((int)error) <= DEADBAND)
-    error = 0.0;
-
-  integral += error * dt;
-  if (integral > anti_windup) integral = anti_windup;
-  if (integral < -anti_windup) integral = -anti_windup;
-
-  derivative = (error - prev_error) / dt;
-
-  float correction = k_p * error + k_i * integral + k_d * derivative;
-
-  // change turn directions so that the car doesnt spin in one single dir
-  if (error > 0) last_turn = 1;
-  if (error < 0) last_turn = -1;
-
-  // updating drive speed according to corrections
-  int current_base = DRIVE_SPEED;
-
-  if (abs((int)error) > 35) {
-    current_base = SEARCH_SPEED;
-  }
-
-  int right_speed = current_base - (int)correction;
-  int left_speed  = current_base + (int)correction;
-
-  drive(right_speed, left_speed);
-
-  prev_error = error;
-}
-
-// ====================== LINE HELPERS ====================
-
-
-void search_line()
+void ON_OFF_CTRL(int ldr)
 {
-  if (last_turn == 1)
-    drive(-RECOVER_SPEED, RECOVER_SPEED);
+  int current_state = (ldr < THRESHOLD) ? 1 : 0;
+
+  if (prev_state ==1 && current_state == 0) 
+    last_turn = !last_turn; // flip direction after each direction loss
+
+  prev_state = current_state;
+
+  if (current_state == 1)
+  {
+    drive(DRIVE_SPEED, DRIVE_SPEED);
+    turn_strength = 100;
+  }
   else
-    drive(RECOVER_SPEED, -RECOVER_SPEED);
+  {
+    turn_strength += 5;
+    if (last_turn == 0)
+      drive(SEARCH_SPEED, turn_strength);
+    else 
+      drive(turn_strength, SEARCH_SPEED);
+  }
 }
 
-bool is_on_line()  { return analogRead(PIN_LDR) < THRESHOLD; }
+void PID_CTRL(int ldr) 
+{
+  error = THRESHOLD - ldr;
+  static float derivative = error - prev_error;
+  integral += error;
+
+  if (integral > 1000) integral = 1000;
+  if (integral < -1000) integral = -1000;
+
+  float control = k_p * error + k_i * integral + k_d * derivative;
+
+  if (error < -120 && abs(derivative) < 5)
+  {
+    // fully lost → force oscillation search
+    control = (prev_error > 0) ? 100 : -100;
+  }
+  
+  if (abs(control) < DEADBAND) control = 0;
+  prev_error = error;
+
+  int base_speed = 140;
+
+  int left_speed  = base_speed + control;
+  int right_speed = base_speed - control;
+
+  // clamp speeds
+  if (left_speed > 255) left_speed = 255;
+  if (left_speed < 0) left_speed = 0;
+
+  if (right_speed > 255) right_speed = 255;
+  if (right_speed < 0) right_speed = 0;
+
+  drive(left_speed, right_speed);
+}
+
 
 // ====================== MOTOR HELPERS ===================
 
@@ -261,7 +208,7 @@ void stop()
 
 int get_distance_cm()
 {
-  // pull trig down high down
+  // pull trig down then high then down
   R_PORT4->PODR &= ~(1 << BIT_TRIG);
   delayMicroseconds(2);
   R_PORT4->PODR |=  (1 << BIT_TRIG);
@@ -276,12 +223,12 @@ int get_distance_cm()
   return (duration * 0.0343) / 2;
 }
 
-// using filtered = α × raw + (1 - α) × previous_filtered
-int read_filtered_sensor()
+
+int ldr_smoothing(void)
 {
-  int raw = analogRead(PIN_LDR);
-  filtered_sensor = alpha * raw + (1.0 - alpha) * filtered_sensor;
-  return (int)filtered_sensor;
+  static float filtered = 0;
+  // RC filter type of thing keep 0.7 of old 0.3 of new measurement
+  filtered = filtered * 0.7 + analogRead(PIN_LDR) * 0.3;
+
+  return (int)filtered;
 }
-
-
